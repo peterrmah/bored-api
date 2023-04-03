a9d96cc2330cdff92c536ed02ada9c5cd2bc4e24
peterrmah <peter.r.mah@gmail.com>
2023-04-03 14:12:23 -0600

    fix: missing enum values & wrong name length

--- server/src/database/migrations/1680326723665-users.ts
+++ server/src/database/migrations/1680326723665-users.ts
@@ -5,7 +5,7 @@ export class users1680326723665 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE \`users\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(50) NOT NULL, \`price\` enum ('Free', 'Low', 'High') NOT NULL, \`accessibility\` enum ('Free', 'Low', 'High') NOT NULL, \`created_date\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+      `CREATE TABLE \`users\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(70) NOT NULL, \`price\` enum ('Free', 'Low', 'High') NOT NULL, \`accessibility\` enum ('Low', 'Medium', 'High') NOT NULL, \`created_date\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
   }
 
