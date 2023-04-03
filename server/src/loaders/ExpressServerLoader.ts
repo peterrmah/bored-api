2dae6cc6f54730caf5a06a9f67504d0b2e4361e5
peterrmah <peter.r.mah@gmail.com>
2023-04-02 22:50:56 -0600

    feat: enable auto validate by class-validator

--- server/src/loaders/ExpressServerLoader.ts
+++ server/src/loaders/ExpressServerLoader.ts
@@ -7,10 +7,23 @@ export function ExpressServerLoader(): Application {
   const expressApp: Application = createExpressServer({
     cors: true,
     classTransformer: true,
-    defaultErrorHandler: false,
     routePrefix: "/api",
     middlewares: [],
     controllers: Object.values(Controllers),
+    validation: true,
+    defaultErrorHandler: true,
+    defaults: {
+      // With this option, null will return 404 by default
+      nullResultCode: 404,
+
+      // With this option, void or Promise<void> will return 204 by default
+      undefinedResultCode: 204,
+
+      paramOptions: {
+        // With this option, argument will be required by default
+        required: true,
+      },
+    },
   });
 
   expressApp.listen(process.env.PORT);
