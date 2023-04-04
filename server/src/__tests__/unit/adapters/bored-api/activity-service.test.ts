7bd5268d662ef065a5224e83125429ea2fba8dba
peterrmah <peter.r.mah@gmail.com>
2023-04-03 19:33:32 -0600

    feat: tests for Price & Accessibility Managers

---
+++ server/src/__tests__/unit/adapters/bored-api/activity-service.test.ts
@@ -0,0 +1,111 @@
+import {
+  AccessibilityManager,
+  PriceManager,
+} from "../../../../adapters/bored-api/activity-service";
+import { AccessibilityLevel, PriceLevel } from "../../../../database/models/user/types";
+
+describe("PriceManager", () => {
+  const priceManager = new PriceManager();
+
+  describe("valueToLevel", () => {
+    const INVALID_MAX_VALUE = 100;
+    const INVALID_MIN_VALUE = -100;
+    const HIGH_VALUE = 1;
+    const LOW_VALUE = 0.5;
+    const FREE = 0;
+
+    test("Throw error when provided value exceeds range max", () => {
+      expect(() => {
+        priceManager.valueToLevel(INVALID_MAX_VALUE);
+      }).toThrow();
+    });
+
+    test("Throw error when provided value is less than range min", () => {
+      expect(() => {
+        priceManager.valueToLevel(INVALID_MIN_VALUE);
+      }).toThrow();
+    });
+
+    test("Correct value to level mappings", () => {
+      expect(priceManager.valueToLevel(HIGH_VALUE)).toEqual(PriceLevel.HIGH);
+      expect(priceManager.valueToLevel(LOW_VALUE)).toEqual(PriceLevel.LOW);
+      expect(priceManager.valueToLevel(FREE)).toEqual(PriceLevel.FREE);
+    });
+  });
+
+  describe("levelToRange", () => {
+    test("Correct FREE price level to range mapping", () => {
+      const DEFINED_RANGE = { min: 0, max: 0 };
+      const actualRange = priceManager.levelToRange(PriceLevel.FREE);
+      expect(actualRange.min).toEqual(DEFINED_RANGE.min);
+      expect(actualRange.max).toEqual(DEFINED_RANGE.max);
+    });
+
+    test("Correct LOW price level to range mapping", () => {
+      const DEFINED_RANGE = { min: 0.1, max: 0.5 };
+      const actualRange = priceManager.levelToRange(PriceLevel.LOW);
+      expect(actualRange.min).toEqual(DEFINED_RANGE.min);
+      expect(actualRange.max).toEqual(DEFINED_RANGE.max);
+    });
+
+    test("Correct HIGH price level to range mapping", () => {
+      const DEFINED_RANGE = { min: 0.6, max: 1 };
+      const actualRange = priceManager.levelToRange(PriceLevel.HIGH);
+      expect(actualRange.min).toEqual(DEFINED_RANGE.min);
+      expect(actualRange.max).toEqual(DEFINED_RANGE.max);
+    });
+  });
+});
+
+describe("AccessibilityManager", () => {
+  const accessibilityManager = new AccessibilityManager();
+
+  describe("valueToLevel", () => {
+    const INVALID_MAX_VALUE = 100;
+    const INVALID_MIN_VALUE = -100;
+    const HIGH_VALUE = 0.25;
+    const MEDIUM_VALUE = 0.75;
+    const LOW_VALUE = 1;
+
+    test("Throw error when provided value exceeds range max", () => {
+      expect(() => {
+        accessibilityManager.valueToLevel(INVALID_MAX_VALUE);
+      }).toThrow();
+    });
+
+    test("Throw error when provided value is less than range min", () => {
+      expect(() => {
+        accessibilityManager.valueToLevel(INVALID_MIN_VALUE);
+      }).toThrow();
+    });
+
+    test("Correct value to level mappings", () => {
+      expect(accessibilityManager.valueToLevel(HIGH_VALUE)).toEqual(AccessibilityLevel.HIGH);
+      expect(accessibilityManager.valueToLevel(MEDIUM_VALUE)).toEqual(AccessibilityLevel.MEDIUM);
+      expect(accessibilityManager.valueToLevel(LOW_VALUE)).toEqual(AccessibilityLevel.LOW);
+    });
+  });
+
+  describe("levelToRange", () => {
+    test("Correct LOW accessibility level to range mapping", () => {
+      const DEFINED_RANGE = { min: 0.76, max: 1 };
+      const actualRange = accessibilityManager.levelToRange(AccessibilityLevel.LOW);
+      expect(actualRange.min).toEqual(DEFINED_RANGE.min);
+      expect(actualRange.max).toEqual(DEFINED_RANGE.max);
+    });
+
+    test("Correct MEDIUM accessibility level to range mapping", () => {
+      const DEFINED_RANGE = { min: 0.26, max: 0.75 };
+      const actualRange = accessibilityManager.levelToRange(AccessibilityLevel.MEDIUM);
+      expect(actualRange.min).toEqual(DEFINED_RANGE.min);
+      expect(actualRange.max).toEqual(DEFINED_RANGE.max);
+    });
+
+    test("Correct HIGH accessibility level to range mapping", () => {
+      const DEFINED_RANGE = { min: 0, max: 0.25 };
+      const actualRange = accessibilityManager.levelToRange(AccessibilityLevel.HIGH);
+      expect(actualRange.min).toEqual(DEFINED_RANGE.min);
+      expect(actualRange.max).toEqual(DEFINED_RANGE.max);
+    });
+  });
+});
